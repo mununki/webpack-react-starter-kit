1311c432519cd2b0f792baaf1e858f75f36bb502
mattdamon108 <woonki.moon@gmail.com>
2019-06-26 22:41:56 +0900

    add routing all to root "/" for SPA

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const webpack = require("webpack");
 const webpackDevMiddleware = require("webpack-dev-middleware");
+const path = require("path");
 
 const app = express();
 const config = require("./webpack.dev.js");
@@ -13,6 +14,19 @@ app.use(
 );
 
 app.use(require("webpack-hot-middleware")(compiler));
+
+// This is for routing all requests to "/" for SPA
+app.get("*", function(req, res, next) {
+  const filename = path.join(compiler.outputPath, "index.html");
+  compiler.outputFileSystem.readFile(filename, function(err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.set("content-type", "text/html");
+    res.send(result);
+    res.end();
+  });
+});
 
 app.listen(3000, function() {
   console.log("Webpack dev server is listening on port 3000");
